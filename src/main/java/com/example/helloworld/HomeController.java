1a90497c871336b190e1946f351303891a70271a
omar0190 <61865216+omar0190@users.noreply.github.com>
2021-01-23 22:52:50 +0100

    a

--- src/main/java/com/example/helloworld/HomeController.java
+++ src/main/java/com/example/helloworld/HomeController.java
@@ -1,6 +1,5 @@
 package com.example.helloworld;
 
-import com.mysql.cj.jdbc.JdbcConnection;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
