a6403520a31f15a4a54df50f7fd86a99d7a93a9c
omar0190 <61865216+omar0190@users.noreply.github.com>
2021-01-23 22:56:01 +0100

    a

--- src/main/java/com/example/helloworld/HomeController.java
+++ src/main/java/com/example/helloworld/HomeController.java
@@ -3,7 +3,6 @@ package com.example.helloworld;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import java.sql.Connection;
 
 @Controller
 public class HomeController {
